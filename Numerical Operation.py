140fd43268c93c25614e06cecfdfa0b674ef54e0
naahe25 <naahebd25@gmail.com>
2023-10-12 22:36:27 +0600

    Use Of Variable

---
+++ Numerical Operation.py
@@ -0,0 +1,31 @@
+ #Numerical Operator
+
+''' 
++ = Addition
+
+- = Substruction
+
+* = Multiplication
+
+/ = Division
+
+% = Modulus ( Remainder )
+
+// = ( FLOOR )
+
+** = Exponentiation (X^Y)
+
+'''
+
+a = 18
+b = 4
+
+print("Addition:" , (a + b) )
+
+print("Substraction:" , (a - b) )
+
+print("Multiplication:" , (a * b) )
+
+print("Division:" , (a / b) )
+
+print("Modulus:" , (a % b) )

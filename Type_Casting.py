d057c79e3a7862b1252fb3f92585e0522451b7ac
naahe25 <naahebd25@gmail.com>
2023-10-14 10:40:02 +0600

    Type Casting

---
+++ Type_Casting.py
@@ -0,0 +1,15 @@
+num1 = input("Enter First Number:")
+num2 = input("Enter Secound Number:")
+
+# sum = (num1 + num2) //This will output together but will not add .For Adding we have to typecast it.
+
+result = int ( num1 ) + int( num2 ) #Typecasting
+
+print("Result Is :",result)
+
+num3 = int ( input("Enter First Number:") )
+num4 = int ( input("Enter Secound Number:") )
+
+result = num3 + num4
+
+print("Result Is:" ,result) # We Use coma (,) for printing Numbers.

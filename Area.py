9c75d056046a05dbc59e148a44b228c16cb26a2b
naahe25 <naahebd25@gmail.com>
2023-10-14 11:09:21 +0600

    Area

---
+++ Area.py
@@ -0,0 +1,19 @@
+# Area Of Any Shape
+
+              #Area Of Triangle
+
+base = float( input ("Enter Base Of Triangle:") )
+height = float ( input("Enter Height Of Triangle:"))
+
+area = 0.5 * base * height
+
+print("Area :" ,area)
+
+                #Area Of Circle
+                
+radius = float( input ("Enter Radius Of Circle:") )
+
+
+area = 3.1416 * radius * radius
+
+print("Area Of Circle :" ,area)

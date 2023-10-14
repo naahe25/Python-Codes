7eee68b0aaf1452b37392db6296eba88e5ead93b
naahe25 <naahebd25@gmail.com>
2023-10-14 11:34:31 +0600

    Library Function Added

---
+++ Library_Function.py
@@ -0,0 +1,18 @@
+# Math Library Function
+from math import *
+
+print((max(20,50))) # we get maximum number Using This Library Function
+
+print((min(20,50))) # we get minimum number Using This Library Function
+
+print((abs(-4))) # we get absolute number Using This Library Function
+
+print((pow(2,3))) # we get Power Number Using This Library Function
+
+print((sqrt(25))) # we get Square Root number Using This Library Function
+
+print((round(3.8))) # we get Round number Using This Library Function
+
+print((floor(3.7))) # we get floor number Using This Library Function
+
+print((ceil(3.7))) # we get Ceil number Using This Library Function
